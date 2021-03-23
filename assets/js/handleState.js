73e8280cb8e156de0370a792087ba305ffd77a40
Henry Chen <hyc@umd.edu>
2021-03-23 17:11:36 -0400

    more calendar work, added breakfast

--- assets/js/handleState.js
+++ assets/js/handleState.js
@@ -13,10 +13,17 @@ function loadInitialState() {
 
       localStorage.setItem("initialized", true);
     }
-      // localStorage.clear();
 }
 
 function loadDefaultEvents() {
+
+  let early_breakfast = {
+    "start_time": "07:00",
+    "end_time": "07:45",
+    "event_name": "Breakfast",
+    "event_type": "meal",
+    "description": "breakfast of champs!",
+  }
 
   let short_lunch = {
     "start_time": "12:00",
@@ -42,6 +49,7 @@ function loadDefaultEvents() {
     "description": "mid day meal baby",
   }
 
+
   let dinner = {
     "start_time": "18:00",
     "end_time": "19:00",
@@ -51,11 +59,11 @@ function loadDefaultEvents() {
   }
 
   let myEvents = {
-    "monday": [short_lunch, dinner],
-    "tuesday": [long_lunch, dinner],
-    "wednesday": [short_lunch, dinner],
-    "thursday": [thursday_lunch, dinner],
-    "friday": [short_lunch, dinner],
+    "monday": [early_breakfast, short_lunch, dinner],
+    "tuesday": [early_breakfast, long_lunch, dinner],
+    "wednesday": [early_breakfast, short_lunch, dinner],
+    "thursday": [early_breakfast, thursday_lunch, dinner],
+    "friday": [early_breakfast, short_lunch, dinner],
   };
 
   localStorage.setItem("events", JSON.stringify(myEvents));
